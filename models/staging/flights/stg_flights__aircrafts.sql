02ef82329d2a0b9d6db31aae7784601db1225e06
SergeyTimofeev <timofeevsergey1989@gmail.com>
2025-06-08 10:45:31 +0300

    add: комментарии к коду

--- models/staging/flights/stg_flights__aircrafts.sql
+++ models/staging/flights/stg_flights__aircrafts.sql
@@ -4,9 +4,8 @@
     )
 }}
 select
-    
     "aircraft_code", 
     "model", 
     "range"
 from
-    {{ source('demo_src', 'aircrafts') }}
+    {{ source('demo_src', 'aircrafts') }} -- demo_src указано в поле name в файле _flights_sources.yml, aircrafts - название таблицы
--- models/staging/flights/stg_flights__bookings.sql
+++ models/staging/flights/stg_flights__bookings.sql
@@ -13,5 +13,10 @@ from
 
 {% if is_incremental() %}
 where
-    book_ref > (select MAX(book_ref) from  {{ this }})
+    book_ref > (select MAX(book_ref) from  {{ this }}) -- Когда используем this то ссылаемся на такую же таблицу(как и имя модели) в БД
 {% endif %}
+
+-- Модель должна существовать в БД
+-- В БД должна существовать таблица соответствующей данной модели
+-- При запуске обновления моделей не передан флаг full-refresh
+-- is_incremental 
