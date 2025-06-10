7c38a20b60cb790832055cdf535e8a9d69a05104
SergeyTimofeev <timofeevsergey1989@gmail.com>
2025-06-10 10:32:31 +0300

    add: первые unit тесты

--- models/intermediate/flights/fct_flights.sql
+++ models/intermediate/flights/fct_flights.sql
@@ -13,6 +13,12 @@ select
     "status",
     "aircraft_code",
     "actual_departure",
-    "actual_arrival"
+    "actual_arrival",
+
+    case
+      when actual_departure is not null and scheduled_departure < actual_departure
+        then actual_departure - scheduled_departure
+        else INTERVAL '0 seconds'
+    end as flight_departure_delay
 from
     {{ ref('stg_flights__flights') }}
